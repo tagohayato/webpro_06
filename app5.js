cc4988d3d5c9a25515c2445705e692f144224440
TAGO Hayato <apghawk@gmail.com>
2024-10-31 20:28:00 +0900

    1029の授業に必要な内容を追加

--- app5.js
+++ app5.js
@@ -29,42 +29,27 @@ app.get("/luck", (req, res) => {
 
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
-  let win = Number(req.query.win);
-  let total = Number(req.query.total);
-  console.log({ hand, win, total });
-  
-  const num = Math.floor(Math.random() * 3 + 1);
+  let win = Number( req.query.win );
+  let total = Number( req.query.total );
+  console.log( {hand, win, total});
+  const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
-  if (num == 1) cpu = 'グー';
-  else if (num == 2) cpu = 'チョキ';
+  if( num==1 ) cpu = 'グー';
+  else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
-
-  // 勝敗の判定
-  let judgement = '';
-  if (hand === cpu) {
-    judgement = 'あいこ';
-  } else if (
-    (hand === 'グー' && cpu === 'チョキ') ||
-    (hand === 'チョキ' && cpu === 'パー') ||
-    (hand === 'パー' && cpu === 'グー')
-  ) {
-    judgement = '勝ち';
-    win += 1;
-  } else {
-    judgement = '負け';
-  }
-  
+  // ここに勝敗の判定を入れる
+  // 今はダミーで人間の勝ちにしておく
+  let judgement = '勝ち';
+  win += 1;
   total += 1;
-
   const display = {
     your: hand,
     cpu: cpu,
     judgement: judgement,
     win: win,
     total: total
-  };
-
-  res.render('janken', display);
+  }
+  res.render( 'janken', display );
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
